19e56f3dc9c6d6f8dad0b23bd5b48ccd9aba0de5
ThuHang14 <14thuhang@gmail.com>
2025-01-09 20:41:35 +0700

    tao loi chao

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-#include <instream>
+#include <iostream>  
 
 using namespace std;
 
 int main()
 {
-cout << "hello wolrd ";
+  
+    cout << "hello world ";  
+    cout << "nguyen viet hoang  ";  
+    cout << "ngay sinh: 24/11 ";  
+    cout << "\n\n----- TIEU SU -----\n";
+    cout << "ho ten: nguyen viet hoang\n";
+    cout << "ngay sinh: 24/11 \n";
+    cout << "nghe nghiep: sinh vien\n";
+    cout << " so thich ban bia da bong trade lap trinh \n";
+    cout << "muc tieu trong tuong lai tro thanh mot lap trinh vien gioi tro thanh mot trader gioi tro thanh con nguoi tot hon .\n";
 
-cout << " nguyen viet hoang ";
-
-cout << " day 11 / mouth 24 / ";
- 
-
- return 0 ;
-   
-   string ()
-   confort_b1 
-
-   for 
-   {
-     cout  << "hoc xinh gioi"
-   
-   
-   }
-
+    return 0;
 }
