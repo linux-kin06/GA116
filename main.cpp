65c76b30b60a59d0edd0c9ffddb087efc533f79d
ThuHang14 <14thuhang@gmail.com>
2025-01-09 20:42:33 +0700

    tao loi chao

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ int main()
     cout << "ngay sinh: 24/11 \n";
     cout << "nghe nghiep: sinh vien\n";
     cout << " so thich ban bia da bong trade lap trinh \n";
-    cout << "muc tieu trong tuong lai tro thanh mot lap trinh vien gioi tro thanh mot trader gioi tro thanh con nguoi tot hon .\n";
-
+    cout << "muc tieu trong tuogg lai tro thanh mot lap trinh vien gioi tro thanh mot trader gioi tro thanh con nguoi tot hon .\n";
+    cout << "2025"
+    
     return 0;
 }
